d5a1be98b64f9b626662d8442bf1321fc39d438f
Glitch (hello-webpage) <none>
2021-01-23 17:29:01 +0000

    📅🍨 Checkpoint ./style.css:8045597/488 ./index.html:8045597/9306 ./script.js:8045597/1223

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var minify = require("html-minifier").minify;
 document.addEventListener("DOMContentLoaded", async () => {
   const node = await window.Ipfs.create({ repo: "ipfs-" + Math.random() });
   window.node = node;
-  document.querySelector().removeAttribute("disabled");
-  document.querySelector().removeAttribute("disabled");
+  document.querySelector("#copybtn").removeAttribute("disabled");
+  document.querySelector("#previewbtn").removeAttribute("disabled");
+  document.querySelector("#downloadbtn").removeAttribute("disabled");
 });
-async function generate(){
-   var html = window.quill.container.firstChild.innerHTML
+async function generate() {
+  var html = window.quill.container.firstChild.innerHTML;
 }
