2c91231234e07896af6c2074666067194527aa67
Glitch (hello-webpage) <none>
2021-01-23 18:49:35 +0000

    🐕🎑 Checkpoint ./style.css:8045597/3820 ./script.js:8045597/2702

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ var minify = require("html-minifier").minify;
 document.addEventListener("DOMContentLoaded", async () => {
   const node = await window.Ipfs.create({ repo: "ipfs-" + Math.random() });
   window.node = node;
-  document.querySelector("#copybtn").removeAttribute("disabled");
-  document.querySelector("#previewbtn").removeAttribute("disabled");
-  document.querySelector("#downloadbtn").removeAttribute("disabled");
+  document.querySelector("header").setAttribute("loaded", "true");
+  document.querySelector("main").setAttribute("loaded", "true");
+  document.querySelector(".loader").setAttribute("loaded", "true");
 });
 async function generate() {
   var html = window.quill.container.firstChild.innerHTML;
+}
+
+function autosave() {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("body", JSON.stringify(quill.getContents()));
+  } else {
+    console.error("Autosave not supported on this browser!");
+  }
+}
+function load() {
+  if (typeof Storage !== "undefined") {
+    quill.setContents(JSON.parse(localStorage.getItem("body")));
+  } else {
+    console.error("Autosave not supported on this browser!");
+  }
 }
