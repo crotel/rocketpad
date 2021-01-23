ffbbcc39cf60c6dc070dff5365fe864a5b25abd5
Glitch (hello-webpage) <none>
2021-01-23 17:05:24 +0000

    🎫🐓 Checkpoint ./lib/ipfs.js:8045597/1906858 ./index.html:8045597/155 ./script.js:8045597/187

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var minify = require("html-minifier").minify;
 document.addEventListener("DOMContentLoaded", async () => {
   const node = await window.Ipfs.create({ repo: "ipfs-" + Math.random() });
   window.node = node;
-  document.getElementById("copy").removeAttribute("disabled");
-  document.getElementById("preview").removeAttribute("disabled");
+  document.querySelector().removeAttribute("disabled");
+  document.querySelector().removeAttribute("disabled");
 });
+async function generate(){
+   var html = window.quill.container.firstChild.innerHTML
+}
